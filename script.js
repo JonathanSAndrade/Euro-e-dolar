51a983f3f7734e54f2f6b44b6cf251f01401b6d0
JonathanSAndrade <jonathan.andrade9.souza@hotmail.com>
2025-07-04 23:58:54 -0300

    Update JS

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const converterButton = document.querySelector('.converter-button');
+
+function convertvalue(){
+    console.log('funcinou?')
+}
+
+converterButton.addEventListener("click", convertvalue);
+
