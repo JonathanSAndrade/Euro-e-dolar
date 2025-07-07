92d2473aa4cd47895b6cd49112e087548772e263
JonathanSAndrade <jonathan.andrade9.souza@hotmail.com>
2025-07-07 18:15:26 -0300

    Update JS currency

--- script.js
+++ script.js
@@ -3,10 +3,22 @@ const converterButton = document.querySelector('.converter-button');
 
 function convertvalue() {
     const inputCurrencyValue = document.querySelector('.input-currency').value;
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); // valor em real
+    const currencyConverter = document.querySelector(".currency-value"); // outras mooedas
 
     const dolarToday = 5.2
 
-    const converterValue = inputCurrencyValue / dolarToday;
+    const converterValue = inputCurrencyValue / dolarToday  
+
+    currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL"
+    }).format(inputCurrencyValue)
+
+    currencyConverter.innerHTML = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD"
+    }).format(converterValue)   
 
     console.log(converterValue);
 
