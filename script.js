31d04fd934f578a333958fe4e163af63c210d742
JonathanSAndrade <jonathan.andrade9.souza@hotmail.com>
2025-07-07 16:53:00 -0300

    Update JS e HTML

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 
 const converterButton = document.querySelector('.converter-button');
 
-function convertvalue(){
-    console.log('funcinou?')
+function convertvalue() {
+    const inputCurrencyValue = document.querySelector('.input-currency').value;
+
+    const dolarToday = 5.2
+
+    const converterValue = inputCurrencyValue / dolarToday;
+
+    console.log(converterValue);
+
 }
 
 converterButton.addEventListener("click", convertvalue);
